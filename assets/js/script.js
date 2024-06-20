d52f52d8686a244cd92df99a192045246bf2c548
tuffythejeep <>
2024-06-20 18:10:04 -0500

    script

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const cityFormEl = document.querySelector("#city-form");
+const historyButtonsEl = document.querySelector("#history-buttons");
+const cityInputEl = document.querySelector("#cityname");
+const weatherContainerEl = document.querySelector("#weather-container");
+const forecastContainerEl = document.querySelector("#forecast-container");
+const citySearchTerm = document.querySelector("#city-search-term");
