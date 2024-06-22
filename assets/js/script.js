b7a5a7be7e30bc79a196dc42e7e99f2d060153d2
tuffythejeep <>
2024-06-21 19:22:46 -0500

    updated styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,26 @@ const cityInputEl = document.querySelector("#cityname");
 const weatherContainerEl = document.querySelector("#weather-container");
 const forecastContainerEl = document.querySelector("#forecast-container");
 const citySearchTerm = document.querySelector("#city-search-term");
+
+const apiKey = "85857869e938505a57be7c6968414eb8";
+
+const formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  if (city) {
+    getCityWeather(city);
+
+    cityInputEl.value = "";
+  } else {
+    alert("Please enter a city name");
+  }
+};
+
+const buttonClickHandler = function (event) {
+  const city = event.target.getAttribute("data-city");
+
+  if (city) {
+    getCityWeather(city);
+  }
+};
+
